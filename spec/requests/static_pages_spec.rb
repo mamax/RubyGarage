e8ade0176279c4d69439e3902a04b3bccbc295a0
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-08-10 13:47:27 +0300

    Add home static page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'SOME TODO LIST'" do
+      visit root_path
+      expect(page).to have_content('SOME TODO LIST')
+    end
+
+    it "should have the title 'Home'" do
+      visit root_path
+      expect(page).to have_title("Ruby on Rails Test App | Home")
+    end
+  end
+end
