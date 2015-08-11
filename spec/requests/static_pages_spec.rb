986039df1fac37a8301a43bd8b45f556cb69cd7c
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-08-11 15:24:08 +0300

    repare tests after deleting home link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,8 +17,6 @@ describe "Static pages" do
     visit root_path
     click_link "Sign in"
     expect(page).to have_title(full_title('Sign in'))
-    click_link "Home"
-    expect(page).to have_title(full_title(''))
     click_link "Sign up now!"
     expect(page).to have_title(full_title(''))
     click_link "Ruby Garage"
